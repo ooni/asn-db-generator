cb83c250bfaff9daabc96a3be15e7a4b3deb1a5f
Simone Basso <bassosimone@gmail.com>
2021-02-15 13:51:30 +0100

    refactor: remove unused code
    This is part of https://github.com/ooni/probe-engine/issues/727

--- mmdb/__init__.py
+++
@@ -1,4 +0,0 @@
-from .mmdb import dump_tree, walk_tree, path_to_network
-from .reader import read_database
-from .types import SearchTreeNode, SearchTreeLeaf
-from .types import Uint16, Uint32, Uint64, Uint128, Int32, Float, Double
